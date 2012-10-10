69455534692589de8fbf3a35fc5991c3bc40dfdb
Gregory Szorc <gps@mozilla.com>
2012-10-10 14:24:35 -0700

    Clean up build.sh; tabs to spaces, add MPL header

--- build.sh
+++ build.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-. setup.sh &&
-time nice -n19 make $MAKE_FLAGS $@
+. setup.sh && time nice -n19 make $MAKE_FLAGS $@
 
 ret=$?
 echo -ne \\a
 if [ $ret -ne 0 ]; then
-	echo
-	echo \> Build failed\! \<
-	echo
-	echo Build with \|./build.sh -j1\| for better messages
-	echo If all else fails, use \|rm -rf objdir-gecko\| to clobber gecko and \|rm -rf out\| to clobber everything else.
+  echo
+  echo \> Build failed\! \<
+  echo
+  echo Build with \|./build.sh -j1\| for better messages
+  echo If all else fails, use \|rm -rf objdir-gecko\| to clobber gecko and \|rm -rf out\| to clobber everything else.
 else
-	if echo $DEVICE | grep generic > /dev/null ; then
-		echo Run \|./run-emulator.sh\| to start the emulator
-		exit 0
-	fi
-	case "$1" in
-	"gecko")
-		echo Run \|./flash.sh gecko\| to update gecko
-		;;
-	*)
-		echo Run \|./flash.sh\| to flash all partitions of your device
-		;;
-	esac
-	exit 0
+  if echo $DEVICE | grep generic > /dev/null ; then
+    echo Run \|./run-emulator.sh\| to start the emulator
+    exit 0
+  fi
+  case "$1" in
+  "gecko")
+    echo Run \|./flash.sh gecko\| to update gecko
+    ;;
+  *)
+    echo Run \|./flash.sh\| to flash all partitions of your device
+    ;;
+  esac
+  exit 0
 fi
 
 exit $ret
