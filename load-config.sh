3127115d8b7fea40be773ecbb771d2412acdaa6e
Gregory Szorc <gps@mozilla.com>
2012-10-10 14:28:11 -0700

    Clean up load-config.sh; MPL header, tabs to spaces

--- load-config.sh
+++ load-config.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 if [[ ! -n "$B2G_DIR" ]]; then
   B2G_DIR=$(cd `dirname $0`; pwd)
@@ -6,10 +9,10 @@ fi
 
 . "$B2G_DIR/.config"
 if [ $? -ne 0 ]; then
-	echo Could not load .config. Did you run config.sh?
-	exit -1
+  echo Could not load .config. Did you run config.sh?
+  exit -1
 fi
 
 if [ -f "$B2G_DIR/.userconfig" ]; then
-	. "$B2G_DIR/.userconfig"
+  . "$B2G_DIR/.userconfig"
 fi
